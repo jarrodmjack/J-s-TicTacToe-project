9bad48dfa10e5014877cdf7f316ff90064f8ccfa
jarrodmjack <jarrodmjack@gmail.com>
2022-04-30 15:25:48 -0400

    adding modal to display outcome to user, adding css, adding restart game button which is on the modal popup

--- js/main.js
+++ js/main.js
@@ -21,8 +21,17 @@
 // let squares = divs.map(item => item.addEventListener('click', function(event){
 //     event.target.innerText = 'X'
 // }))
+
+
+// tracker for how determining which piece is in play 
 let counter = 0
 let isX = true
+
+// modal stuff
+let popup = document.querySelector(".popup"),
+button = document.querySelector("#restart");
+
+
 
 
 class TicTacToe {
@@ -31,59 +40,118 @@ class TicTacToe {
     this.board = Array.from(document.querySelectorAll('div'))
     this.board.forEach(item => item.addEventListener('click', this.handlePlayer))
     // this.board.forEach(item => item.addEventListener('click', this.checkWin))
-
-
   }
 
+
+  
+// Win conditions
   checkWin = () => {
 
 
-
-
     if (this.board[0].innerText === this.board[1].innerText && this.board[1].innerText === this.board[2].innerText && this.board[0].innerText) {
-      alert(`${this.board[0].innerText} wins!`)
+      console.log(`${this.board[0].innerText} wins!`)
+      document.querySelector('#winner').innerText = `${this.board[0].innerText} wins!`
+      modal.openPop()
+      document.querySelector('#restart').addEventListener('click', this.restart)
+  
     } 
     else if (this.board[3].innerText === this.board[4].innerText && this.board[4].innerText === this.board[5].innerText && this.board[3].innerText) {
-      alert(`${this.board[3].innerText} wins!`)
+      console.log(`${this.board[3].innerText} wins!`)
+      console.log(`${this.board[3].innerText} wins!`)
+      document.querySelector('#winner').innerText = `${this.board[3].innerText} wins!`
+      modal.openPop()
+      document.querySelector('#restart').addEventListener('click', this.restart)
     }
      else if (this.board[6].innerText === this.board[7].innerText && this.board[7].innerText === this.board[8].innerText && this.board[6].innerText) {
-      alert(`${this.board[6].innerText} wins!`)
+      console.log(`${this.board[6].innerText} wins!`)
+      console.log(`${this.board[6].innerText} wins!`)
+      document.querySelector('#winner').innerText = `${this.board[6].innerText} wins!`
+      modal.openPop()
+      document.querySelector('#restart').addEventListener('click', this.restart)
     }
      else if (this.board[2].innerText === this.board[4].innerText && this.board[4].innerText === this.board[6].innerText && this.board[2].innerText) {
-      alert(`${this.board[2].innerText} wins!`)
+      console.log(`${this.board[2].innerText} wins!`)
+      console.log(`${this.board[2].innerText} wins!`)
+      document.querySelector('#winner').innerText = `${this.board[2].innerText} wins!`
+      modal.openPop()
+      document.querySelector('#restart').addEventListener('click', this.restart)
     }
      else if (this.board[0].innerText === this.board[4].innerText && this.board[4].innerText === this.board[8].innerText && this.board[0].innerText) {
-      alert(`${this.board[0].innerText} wins!`)
+      console.log(`${this.board[0].innerText} wins!`)
+      console.log(`${this.board[0].innerText} wins!`)
+      document.querySelector('#winner').innerText = `${this.board[0].innerText} wins!`
+      modal.openPop()
+      document.querySelector('#restart').addEventListener('click', this.restart)
     }
      else if (this.board[0].innerText === this.board[3].innerText && this.board[3].innerText === this.board[6].innerText && this.board[0].innerText) {
-      alert(`${this.board[0].innerText} wins!`)
+      console.log(`${this.board[0].innerText} wins!`)
+      console.log(`${this.board[0].innerText} wins!`)
+      document.querySelector('#winner').innerText = `${this.board[0].innerText} wins!`
+      modal.openPop()
+      document.querySelector('#restart').addEventListener('click', this.restart)
     }
      else if (this.board[1].innerText === this.board[4].innerText && this.board[4].innerText === this.board[7].innerText && this.board[1].innerText) {
-      alert(`${this.board[1].innerText} wins!`)
+      console.log(`${this.board[1].innerText} wins!`)
+      console.log(`${this.board[1].innerText} wins!`)
+      document.querySelector('#winner').innerText = `${this.board[1].innerText} wins!`
+      modal.openPop()
+      document.querySelector('#restart').addEventListener('click', this.restart)
     }
      else if (this.board[2].innerText === this.board[5].innerText && this.board[5].innerText === this.board[8].innerText && this.board[2].innerText) {
-      alert(`${this.board[2].innerText} wins!`)
-    }else{
+      console.log(`${this.board[2].innerText} wins!`)
+      console.log(`${this.board[2].innerText} wins!`)
+      document.querySelector('#winner').innerText = `${this.board[2].innerText} wins!`
+      modal.openPop()
+      document.querySelector('#restart').addEventListener('click', this.restart)
+    }else{ //DRAW CONDITION
       counter++
       if(counter === 9){
-        document.querySelector('h2').innerText = 'tie game'
+        document.querySelector('#winner').innerText = 'tie game'
+        modal.openPop()
+        document.querySelector('#restart').addEventListener('click', this.restart)
       }
       
     }
+
+
+
   }
 
 
+  // restart game
+
+  // temporary restart until I can figure out how to actually restart the game lol
+
+  restart = (e) => {
+    window.location.reload()
+    // this.board.forEach(item => item.innerText = '')
+    // popup.style.display = "none";
+    // counter = 0
+    // isX = true
+
+    // set isX to false
+    // text must be erased
+    // counter gets set to 0
+    // modal closes
+  }
+
+
+  // determining player piece. Alternates between X's and O's
   handlePlayer = (e) => {
 
     if(e.target.innerText){
-      return alert('cant place here')
+      return
     }
 
     if (isX) {
       e.target.innerText = 'X'
+      e.target.style.color = '#e31170'
+      e.target.style.fontSize = '7rem'
       isX = false
     } else {
       e.target.innerText = 'O'
+      e.target.style.color = '#c89731'
+      e.target.style.fontSize = '7rem'
       isX = true
     }
     this.checkWin()
@@ -109,9 +177,41 @@ let board1 = new TicTacToe()
 
 
 
+// modal
 
 
 
+let modal = {
+ 
+
+
+    openPop(){
+      popup.style.display = "Block";
+    },
+
+
+
+  //  closePop(e){
+  //     if(e.target == popup) {
+  //     popup.style.display = "none";
+  //  }
+
+}
+
+
+
+// button.addEventListener("click", openPop);
+
+
+
+// Add restart button to this
+// window.addEventListener("click", closePop);
+
+// closePop(e){
+//   if(e.target == popup) {
+//   popup.style.display = "none";
+// }
+// }
 
 
 
