52acd791badba32dff8205527c6e5587a2ee858c
jarrodmjack <jarrodmjack@gmail.com>
2022-05-02 22:17:14 -0400

    adding local storage to save players score into local storage across sessions. Adding score display into DOM

--- js/main.js
+++ js/main.js
@@ -11,25 +11,35 @@
 
 
 
-
-
-// let divs = Array.from(document.querySelectorAll('div'))
-
-// let counter = 0
-
-
-// let squares = divs.map(item => item.addEventListener('click', function(event){
-//     event.target.innerText = 'X'
-// }))
-
-
 // tracker for how determining which piece is in play 
 let counter = 0
 let isX = true
 
 // modal stuff
 let popup = document.querySelector(".popup"),
-button = document.querySelector("#restart");
+  button = document.querySelector("#restart");
+
+
+
+  // local storage
+  if(!localStorage.getItem('xScore')){
+    localStorage.setItem('xScore', 0)
+  }
+
+  if(!localStorage.getItem('oScore')){
+    localStorage.setItem('oScore', 0)
+  }
+
+  let scoreOfX = Number(localStorage.getItem('xScore'))
+  let scoreOfO = Number(localStorage.getItem('oScore'))
+
+
+  // Display for DOM scores
+  let x = document.querySelector('#xScore')
+  let o = document.querySelector('#oScore')
+
+  x.innerText = localStorage.getItem('xScore')
+  o.innerText = localStorage.getItem('oScore')
 
 
 
@@ -43,74 +53,226 @@ class TicTacToe {
   }
 
 
-  
-// Win conditions
+
+  // Win conditions
   checkWin = () => {
 
 
     if (this.board[0].innerText === this.board[1].innerText && this.board[1].innerText === this.board[2].innerText && this.board[0].innerText) {
       console.log(`${this.board[0].innerText} wins!`)
+
+      // adding local storage scores
+    
+      if(this.board[0].innerText === 'X'){
+        scoreOfX += 1
+      }else if(this.board[0].innerText === 'O'){
+        scoreOfO += 1
+      }
+      localStorage.setItem('xScore', scoreOfX)
+      localStorage.setItem('oScore', scoreOfO)
+
+
+      //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+      
+
       document.querySelector('#winner').innerText = `${this.board[0].innerText} wins!`
       modal.openPop()
       document.querySelector('#restart').addEventListener('click', this.restart)
-  
-    } 
+
+
+    
+    }
     else if (this.board[3].innerText === this.board[4].innerText && this.board[4].innerText === this.board[5].innerText && this.board[3].innerText) {
       console.log(`${this.board[3].innerText} wins!`)
-      console.log(`${this.board[3].innerText} wins!`)
+      
+
+      // adding local storage scores
+       if(this.board[3].innerText === 'X'){
+        scoreOfX += 1
+      }else if(this.board[3].innerText === 'O'){
+        scoreOfO += 1
+      }
+      localStorage.setItem('xScore', scoreOfX)
+      localStorage.setItem('oScore', scoreOfO)
+
+     //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+
+
       document.querySelector('#winner').innerText = `${this.board[3].innerText} wins!`
       modal.openPop()
       document.querySelector('#restart').addEventListener('click', this.restart)
+
+
     }
-     else if (this.board[6].innerText === this.board[7].innerText && this.board[7].innerText === this.board[8].innerText && this.board[6].innerText) {
+    else if (this.board[6].innerText === this.board[7].innerText && this.board[7].innerText === this.board[8].innerText && this.board[6].innerText) {
       console.log(`${this.board[6].innerText} wins!`)
-      console.log(`${this.board[6].innerText} wins!`)
+
+      
+// adding local storage scores
+      if(this.board[6].innerText === 'X'){
+        scoreOfX += 1
+      }else if(this.board[6].innerText === 'O'){
+        scoreOfO += 1
+      }
+      localStorage.setItem('xScore', scoreOfX)
+      localStorage.setItem('oScore', scoreOfO)
+
+           //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+
+
+
       document.querySelector('#winner').innerText = `${this.board[6].innerText} wins!`
       modal.openPop()
       document.querySelector('#restart').addEventListener('click', this.restart)
+      
     }
-     else if (this.board[2].innerText === this.board[4].innerText && this.board[4].innerText === this.board[6].innerText && this.board[2].innerText) {
+    else if (this.board[2].innerText === this.board[4].innerText && this.board[4].innerText === this.board[6].innerText && this.board[2].innerText) {
       console.log(`${this.board[2].innerText} wins!`)
-      console.log(`${this.board[2].innerText} wins!`)
+
+      
+      // adding local storage scores
+      if(this.board[2].innerText === 'X'){
+        scoreOfX += 1
+      }else if(this.board[2].innerText === 'O'){
+        scoreOfO += 1
+      }
+      localStorage.setItem('xScore', scoreOfX)
+      localStorage.setItem('oScore', scoreOfO)
+
+
+           //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+
+
       document.querySelector('#winner').innerText = `${this.board[2].innerText} wins!`
       modal.openPop()
       document.querySelector('#restart').addEventListener('click', this.restart)
     }
-     else if (this.board[0].innerText === this.board[4].innerText && this.board[4].innerText === this.board[8].innerText && this.board[0].innerText) {
+
+    else if (this.board[0].innerText === this.board[4].innerText && this.board[4].innerText === this.board[8].innerText && this.board[0].innerText) {
       console.log(`${this.board[0].innerText} wins!`)
-      console.log(`${this.board[0].innerText} wins!`)
+      
+
+      // adding local storage scores
+      if(this.board[0].innerText === 'X'){
+        scoreOfX += 1
+      }else if(this.board[0].innerText === 'O'){
+        scoreOfO += 1
+      }
+      localStorage.setItem('xScore', scoreOfX)
+      localStorage.setItem('oScore', scoreOfO)
+
+
+           //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+
+
       document.querySelector('#winner').innerText = `${this.board[0].innerText} wins!`
       modal.openPop()
       document.querySelector('#restart').addEventListener('click', this.restart)
     }
-     else if (this.board[0].innerText === this.board[3].innerText && this.board[3].innerText === this.board[6].innerText && this.board[0].innerText) {
+
+    else if (this.board[0].innerText === this.board[3].innerText && this.board[3].innerText === this.board[6].innerText && this.board[0].innerText) {
       console.log(`${this.board[0].innerText} wins!`)
-      console.log(`${this.board[0].innerText} wins!`)
+      
+
+      // adding local storage scores
+      if(this.board[0].innerText === 'X'){
+        scoreOfX += 1
+      }else if(this.board[0].innerText === 'O'){
+        scoreOfO += 1
+      }
+      localStorage.setItem('xScore', scoreOfX)
+      localStorage.setItem('oScore', scoreOfO)
+
+
+           //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+
+
       document.querySelector('#winner').innerText = `${this.board[0].innerText} wins!`
       modal.openPop()
       document.querySelector('#restart').addEventListener('click', this.restart)
     }
-     else if (this.board[1].innerText === this.board[4].innerText && this.board[4].innerText === this.board[7].innerText && this.board[1].innerText) {
+
+    else if (this.board[1].innerText === this.board[4].innerText && this.board[4].innerText === this.board[7].innerText && this.board[1].innerText) {
       console.log(`${this.board[1].innerText} wins!`)
-      console.log(`${this.board[1].innerText} wins!`)
+      
+
+      // adding local storage scores
+      if(this.board[1].innerText === 'X'){
+        scoreOfX += 1
+      }else if(this.board[1].innerText === 'O'){
+        scoreOfO += 1
+      }
+      localStorage.setItem('xScore', scoreOfX)
+      localStorage.setItem('oScore', scoreOfO)
+
+
+           //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+
+
       document.querySelector('#winner').innerText = `${this.board[1].innerText} wins!`
       modal.openPop()
       document.querySelector('#restart').addEventListener('click', this.restart)
     }
-     else if (this.board[2].innerText === this.board[5].innerText && this.board[5].innerText === this.board[8].innerText && this.board[2].innerText) {
+
+    else if (this.board[2].innerText === this.board[5].innerText && this.board[5].innerText === this.board[8].innerText && this.board[2].innerText) {
+
       console.log(`${this.board[2].innerText} wins!`)
-      console.log(`${this.board[2].innerText} wins!`)
+      
+      
+      // adding local storage scores
+      if(this.board[2].innerText === 'X'){
+        scoreOfX += 1
+      }else if(this.board[2].innerText === 'O'){
+        scoreOfO += 1
+      }
+      localStorage.setItem('xScore', scoreOfX)
+      localStorage.setItem('oScore', scoreOfO)
+
+
+           //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+
+
       document.querySelector('#winner').innerText = `${this.board[2].innerText} wins!`
       modal.openPop()
       document.querySelector('#restart').addEventListener('click', this.restart)
-    }else{ //DRAW CONDITION
+    } 
+    
+    else { //DRAW CONDITION
       counter++
-      if(counter === 9){
+      if (counter === 9) {
+
+          scoreOfX += 0
+          scoreOfO += 0
+      
+        localStorage.setItem('xScore', scoreOfX)
+        localStorage.setItem('oScore', scoreOfO)
+
+             //putting scores into DOM
+     x.innerText = localStorage.getItem('xScore')
+     o.innerText = localStorage.getItem('oScore')
+
+
         document.querySelector('#winner').innerText = 'tie game'
         modal.openPop()
         document.querySelector('#restart').addEventListener('click', this.restart)
       }
-      
+
     }
 
 
@@ -119,27 +281,17 @@ class TicTacToe {
 
 
   // restart game
-
-  // temporary restart until I can figure out how to actually restart the game lol
-
+  // this just reloads the entire window. 
   restart = (e) => {
     window.location.reload()
-    // this.board.forEach(item => item.innerText = '')
-    // popup.style.display = "none";
-    // counter = 0
-    // isX = true
 
-    // set isX to false
-    // text must be erased
-    // counter gets set to 0
-    // modal closes
   }
 
 
-  // determining player piece. Alternates between X's and O's
+  // determining player piece. Alternates between X's and O's. Also added some x / o specific styles :)
   handlePlayer = (e) => {
 
-    if(e.target.innerText){
+    if (e.target.innerText) {
       return
     }
 
@@ -158,38 +310,24 @@ class TicTacToe {
 
   }
 
-  // // placing player pieces
-
-  // placePiece(event){
-  //   this.event = document.querySelector('#3')
-  // }
 }
 
 
 let board1 = new TicTacToe()
 
 
-// class Player{
-//   constructor(piece){
-//     this.piece = piece
-//   }
-// }
-
-
-
-// modal
-
 
 
 let modal = {
- 
 
 
-    openPop(){
-      popup.style.display = "Block";
-    },
+
+  openPop() {
+    popup.style.display = "Block";
+  },
 
 
+  // closing modal not required
 
   //  closePop(e){
   //     if(e.target == popup) {
@@ -197,21 +335,6 @@ let modal = {
   //  }
 
 }
-
-
-
-// button.addEventListener("click", openPop);
-
-
-
-// Add restart button to this
-// window.addEventListener("click", closePop);
-
-// closePop(e){
-//   if(e.target == popup) {
-//   popup.style.display = "none";
-// }
-// }
 
 
 
